7f0e8f9c52edfd02acd6ee807ce0f122d0caac7e
87tawanzinho <87tawanzinho@gmail.com>
2023-12-16 07:43:13 +0000

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,125 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+/*
+* CHALLENGE progresso do formulário
+
+* INSTRUÇÕES
+Neste desafio sua missão é criar um formulário e seus 4 campos (com controlled inputs),
+juntamente com uma barra de progresso que altera-se conforme o usuário preenche os campos.
+- Crie também validações para cada campo conforme instruções abaixo.
+
+* BARRA DE PROGRESSO
+Para aproveitar estilização já definida, crie:
+- a barra com um elemento pai chamado .bar-container e seu filho .bar
+
+* CAMPOS DO FORMULÁRIO:
+input - nome completo - válido se digitar no mínimo dois nomes,
+input - email - válido se digitar um e-mail,
+select - estado civil,
+radio - gênero
+
+Para validação de e-mail use a seguinte RegEx: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+* FUNCIONAMENTO
+Espera-se que o formulário tenha 4 campos ao todo. Portanto, quando o usuário preencher
+o primeiro campo, a barra de progresso deve assumir 25% do tamanho total;
+o segundo campo, 50% e assim por diante...
+
+Caso o usuário não tenha definido valores para os elementos de select e radio,
+os mesmos não devem ser considerados como preenchidos até então.
+
+Se o usuário preencher um campo e apagar seu valor, este campo deve ser considerado como vazio,
+fazendo com que a barra de progresso regrida novamente.
+
+Desabilitar o botão de enviar caso todos os campos não estejam preenchidos/válidos.
+
+Ao enviar, deve-se apresentar um alert javascript com sucesso, limpar todos os campos
+do formulário e zerar a barra de progresso novamente.
+*/
+"use client";
+import { useState } from "react";
+import { data, returnData } from "./types";
 
 export default function Home() {
+  const [data, setData] = useState<data>({
+    fullName: "",
+    email: "",
+    maritalStatus: "",
+    genre: null,
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+    console.log(name, value);
+  };
+
+  const handleRadioChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newData = event.target.value;
+    setData((prev) => ({
+      ...prev,
+
+      genre: event.target.value as null | "masculino" | "feminino",
+    }));
+
+    console.log(newData);
+  };
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="App">
+      <h3>desafio fernandev</h3>
+      <h1>progresso do formulário</h1>
+
+      <main>
+        {/* crie a barra de progresso aqui */}
+        <div className="form-group">
+          <label htmlFor="">Nome Completo</label>
+          <input
+            name="fullName"
+            value={data.fullName}
+            onChange={handleChange}
+          />
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+        <div className="form-group">
+          <label htmlFor="">E-mail</label>
+          <input name="email" value={data.email} onChange={handleChange} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="">Estado Civil</label>
+          <select name="maritalStatus" value={data.maritalStatus}>
+            <option value="">- selecione...</option>
+            <option value="solteiro">Solteiro</option>
+            <option value="casado">Casado</option>
+            <option value="divorciado">Divorciado</option>
+          </select>
+        </div>
+        <div className="form-group">
+          <label htmlFor="">Gênero</label>
+          <div className="radios-container">
+            <span>
+              <input
+                type="radio"
+                name="genre"
+                value="masculino"
+                checked={data.genre === "masculino"}
+                onChange={handleRadioChange}
+              />{" "}
+              Masculino
+            </span>
+            <span>
+              <input
+                type="radio"
+                name="genre"
+                value="feminino"
+                checked={data.genre === "feminino"}
+                onChange={handleRadioChange}
+              />{" "}
+              Feminino
+            </span>
+          </div>
+        </div>
+        <button>Enviar Formulário</button>
+      </main>
+    </div>
+  );
 }
---
+++ app/types.tsx
@@ -0,0 +1,10 @@
+export type data = {
+  fullName: string;
+  email: string;
+  maritalStatus: string;
+  genre: null | "masculino" | "feminino";
+};
+
+export type returnData = {
+  obj: data;
+};
